ebc14a364e4769ab5d8b42f86c1ffc3fce35581c
Miklosoft <57501210+Miklosoft@users.noreply.github.com>
2020-06-24 15:50:50 +0300

    Update ProPetsAccountingApiApplication.java

--- src/main/java/accounting/ProPetsAccountingApiApplication.java
+++ src/main/java/accounting/ProPetsAccountingApiApplication.java
@@ -3,6 +3,7 @@ package accounting;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Here was Mikael. Find this comment. :-)
 @SpringBootApplication
 public class ProPetsAccountingApiApplication {
 
